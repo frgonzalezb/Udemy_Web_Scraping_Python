28dff94acb95c6e37bed709be7aae41a05250221
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-08 16:45:08 -0400

    Contruyendo el bot AudibleBot

---
+++ 06_Proyecto1_AudibleBot/script.py
@@ -0,0 +1,87 @@
+"""
+Proyecto 1: Audible Bot
+
+Página a usar: https://www.audible.com/search
+"""
+
+
+import os
+import time
+
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement  # 4 typing
+from selenium.common.exceptions import NoSuchElementException
+
+
+URL: str = 'https://www.audible.com/search'
+
+
+def get_element_as_text(
+        web_element: WebElement,
+        tag: str,
+        class_name: str
+        ) -> str:
+    """
+    Extracts the text from an element, if any.
+    """
+    selector: str = f'.//{tag}[contains(@class, "{class_name}")]'
+    try:
+        return web_element.find_element(By.XPATH, selector).text
+    except NoSuchElementException:
+        return f'WARNING: {class_name} element not found.'
+
+
+def run_script() -> None:
+    driver: webdriver.Chrome = webdriver.Chrome()
+    driver.get(URL)
+
+    container: WebElement = driver.find_element(
+        By.CLASS_NAME,
+        'adbl-impression-container'
+    )
+
+    products: list[WebElement] = container.find_elements(
+        By.XPATH,
+        './/li[contains(@class, "productListItem")]'
+    )
+
+    book_titles: list[str] = []
+    book_authors: list[str] = []
+    book_runtimes: list[str] = []
+
+    for product in products:
+        title: str = get_element_as_text(product, 'h3', 'bc-heading')
+        book_titles.append(title)
+
+        author: str = get_element_as_text(product, 'li', 'authorLabel')
+        author = author.split('De: ')[-1]
+        book_authors.append(author)
+
+        runtime: str = get_element_as_text(product, 'li', 'runtimeLabel')
+        runtime = runtime.split('Duración: ')[-1]
+        book_runtimes.append(runtime)
+
+        time.sleep(3)
+
+    driver.quit()
+
+    df: pd.DataFrame = pd.DataFrame({
+        'Title': book_titles,
+        'Author': book_authors,
+        'Runtime': book_runtimes
+    })
+
+    base_dir: str = './06_Proyecto1_AudibleBot/res/'
+    filename: str = 'books.csv'
+
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+
+    df.to_csv(base_dir + filename, index=False)
+
+
+if __name__ == '__main__':
+    run_script()
