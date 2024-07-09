b30c55d6a27f8343c306b9c534dc15ad2fcd3420
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-09 12:24:47 -0400

    Creando Primer Proyecto y Spider

---
+++ 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class WorldometersSpider(scrapy.Spider):
+    name = "worldometers"
+    allowed_domains = ["www.worldometers.info"]
+    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+
+    def parse(self, response):
+        pass
