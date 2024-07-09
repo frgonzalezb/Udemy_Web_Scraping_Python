b5ba2f3b676183e84cfd6ee6bc1f020232955161
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-09 13:34:18 -0400

    Links Relativos vs Absolutos

--- 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
+++ 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
@@ -15,15 +15,17 @@ class WorldometersSpider(scrapy.Spider):
     def parse(
             self,
             response: HtmlResponse
-            ) -> Generator[dict[str, Any], Any, None]:
+            ) -> Generator[scrapy.Request, Any, None]:
         # title: str | None = response.xpath('//h1/text()').get()
         countries: SelectorList[Selector] = response.xpath('//td/a')
 
         for country in countries:
-            name: str | None = country.xpath('.//text()').get()
+            # name: str | None = country.xpath('.//text()').get()
             link: str | None = country.xpath('.//@href').get()
 
-            yield {
-                'name': name,
-                'link': link
-            }
+            # trabajando con links absolutos
+            # abs_url: str = f'https://{self.allowed_domains[0]}{link}'
+            # abs_url: str = response.urljoin(link)
+
+            # trabajando con links relativos
+            yield response.follow(url=link)
