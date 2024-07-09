a8ed64343b0b10b2375114097f491bb20feb4c2f
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-09 17:05:52 -0400

    Extraer Data de Multiples Links

--- 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
+++ 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
@@ -16,11 +16,14 @@ class WorldometersSpider(scrapy.Spider):
             self,
             response: HtmlResponse
             ) -> Generator[scrapy.Request, Any, None]:
+        """
+        Parses the HTML response from the population-by-country URL.
+        """
         # title: str | None = response.xpath('//h1/text()').get()
         countries: SelectorList[Selector] = response.xpath('//td/a')
 
         for country in countries:
-            # name: str | None = country.xpath('.//text()').get()
+            name: str | None = country.xpath('.//text()').get()
             link: str | None = country.xpath('.//@href').get()
 
             # trabajando con links absolutos
@@ -28,4 +31,29 @@ class WorldometersSpider(scrapy.Spider):
             # abs_url: str = response.urljoin(link)
 
             # trabajando con links relativos
-            yield response.follow(url=link)
+            yield response.follow(
+                url=link,
+                callback=self.parse_country,
+                meta={'country': name}
+            )
+
+    def parse_country(
+            self,
+            response: HtmlResponse
+            ) -> Generator[dict[str, str | None], Any, None]:
+        """
+        Parses the HTML response from each country population URL.
+        """
+        country = response.request.meta['country']
+        xpath: str = '(//table[contains(@class, "table")])[1]/tbody/tr'
+        rows: SelectorList[Selector] = response.xpath(xpath)
+
+        for row in rows:
+            year: str | None = row.xpath('.//td[1]/text()').get()
+            population: str | None = row.xpath('.//td[2]/strong/text()').get()
+
+            yield {
+                'country': country,
+                'year': year,
+                'population': population
+            }
