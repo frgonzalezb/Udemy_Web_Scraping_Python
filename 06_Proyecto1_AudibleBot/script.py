8d7c6bf3e4a2adcbe4ac9e376a9cd7660b4fc4bb
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-08 21:13:45 -0400

    Paginación

--- 06_Proyecto1_AudibleBot/script.py
+++ 06_Proyecto1_AudibleBot/script.py
@@ -17,7 +17,14 @@ from selenium.webdriver.remote.webelement import WebElement  # 4 typing
 from selenium.common.exceptions import NoSuchElementException
 
 
-URL: str = 'https://www.audible.com/search'
+def get_driver() -> webdriver.Chrome:
+    options: Options = Options()
+    # options.headless = True  # DEPRECADO!!
+    # https://www.selenium.dev/blog/2023/headless-is-going-away/
+    options.add_argument("--headless=new")
+
+    driver: webdriver.Chrome = webdriver.Chrome(options=options)
+    return driver
 
 
 def get_element_as_text(
@@ -35,51 +42,14 @@ def get_element_as_text(
         return f'WARNING: {class_name} element not found.'
 
 
-def run_script() -> None:
-    options: Options = Options()
-    # options.headless = True  # DEPRECADO!!
-    # https://www.selenium.dev/blog/2023/headless-is-going-away/
-    options.add_argument("--headless=new")
-
-    driver: webdriver.Chrome = webdriver.Chrome(options=options)
-    driver.get(URL)
-
-    container: WebElement = driver.find_element(
-        By.CLASS_NAME,
-        'adbl-impression-container'
-    )
-
-    products: list[WebElement] = container.find_elements(
-        By.XPATH,
-        './/li[contains(@class, "productListItem")]'
-    )
-
-    book_titles: list[str] = []
-    book_authors: list[str] = []
-    book_runtimes: list[str] = []
-
-    for product in products:
-        title: str = get_element_as_text(product, 'h3', 'bc-heading')
-        book_titles.append(title)
-
-        author: str = get_element_as_text(product, 'li', 'authorLabel')
-        author = author.split('De: ')[-1]
-        book_authors.append(author)
-
-        runtime: str = get_element_as_text(product, 'li', 'runtimeLabel')
-        runtime = runtime.split('Duración: ')[-1]
-        book_runtimes.append(runtime)
-
-        print(f'\n\t{title=}, \n\t{author=}, \n\t{runtime=}')  # dbg
-
-        time.sleep(1)
-
-    driver.quit()
-
+def create_csv_file(data: tuple[list[str]]) -> None:
+    """
+    Creates a CSV file with the extracted data.
+    """
     df: pd.DataFrame = pd.DataFrame({
-        'Title': book_titles,
-        'Author': book_authors,
-        'Runtime': book_runtimes
+        'Title': data[0],
+        'Author': data[1],
+        'Runtime': data[2]
     })
 
     print('\nDataframe created successfully!', '\n', df)
@@ -91,6 +61,102 @@ def run_script() -> None:
         os.mkdir(base_dir)
 
     df.to_csv(base_dir + filename, index=False)
+
+
+def run_script() -> None:
+    url: str = 'https://www.audible.com/es_US/search'
+
+    driver = get_driver()
+    driver.get(url)
+
+    pagination: WebElement = driver.find_element(
+        By.XPATH,
+        './/ul[contains(@class, "pagingElements")]'
+    )
+    pages: list[WebElement] = pagination.find_elements(By.TAG_NAME, 'li')
+    last_page: int = int(pages[-2].text)
+
+    book_titles: list[str] = []
+    book_authors: list[str] = []
+    book_runtimes: list[str] = []
+
+    """
+    NOTA PARA MI YO DEL FUTURO: El modo de Frank ya no funciona y he
+    decidido hacerlo como en el script de BeautifulSoup, mediante el uso
+    de params. Dejaré el estilo de Frank comentado más abajo.
+    """
+
+    current_page: int = 1
+    params: str = f'?page={current_page}'
+    for page in range(1, last_page + 1)[:2]:  # solo 2 primeras páginas
+        print(f'\nCurrent page: {page}/{last_page}')
+        driver.get(url + params)
+
+        container: WebElement = driver.find_element(
+            By.CLASS_NAME,
+            'adbl-impression-container'
+        )
+
+        products: list[WebElement] = container.find_elements(
+            By.XPATH,
+            './/li[contains(@class, "productListItem")]'
+        )
+
+        for product in products:
+            title: str = get_element_as_text(product, 'h3', 'bc-heading')
+            book_titles.append(title)
+
+            author: str = get_element_as_text(product, 'li', 'authorLabel')
+            author = author.split('De: ')[-1]
+            book_authors.append(author)
+
+            runtime: str = get_element_as_text(product, 'li', 'runtimeLabel')
+            runtime = runtime.split('Duración: ')[-1]
+            book_runtimes.append(runtime)
+
+            print(f'\n\t{title=}, \n\t{author=}, \n\t{runtime=}')  # dbg
+
+            time.sleep(2)
+
+        current_page += 1
+        params = f'?page={current_page}'
+
+    # while current_page <= last_page:
+    #     print(f'\nCurrent page: {current_page}/{last_page}')
+
+    #     for product in products:
+    #         title: str = get_element_as_text(product, 'h3', 'bc-heading')
+    #         book_titles.append(title)
+
+    #         author: str = get_element_as_text(product, 'li', 'authorLabel')
+    #         author = author.split('De: ')[-1]
+    #         book_authors.append(author)
+
+    #         runtime: str = get_element_as_text(product, 'li', 'runtimeLabel')
+    #         runtime = runtime.split('Duración: ')[-1]
+    #         book_runtimes.append(runtime)
+
+    #         print(f'\n\t{title=}, \n\t{author=}, \n\t{runtime=}')  # dbg
+
+    #         time.sleep(1)
+
+    #     try:
+    #         next_page: WebElement = driver.find_element(
+    #             By.XPATH,
+    #             './/span[contains(@class, "nextButton")]'
+    #         )
+    #         next_page.click()
+    #     except NoSuchElementException:
+    #         print('Last page reached!')
+    #         break
+
+    #     current_page += 1
+    #     time.sleep(3)
+
+    driver.quit()
+
+    create_csv_file((book_titles, book_authors, book_runtimes))
+
     print('\nThis script has finished its job successfully!')
 
 
