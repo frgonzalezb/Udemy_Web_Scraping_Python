9c3468f96066de0d5551e1eefb3718404238e099
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-09 13:08:01 -0400

    Construyendo el Spider

--- 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
+++ 07_IntroScrapy/tutorial/tutorial/spiders/worldometers.py
@@ -1,10 +1,24 @@
+from typing import Any, Generator
+
 import scrapy
+from scrapy.http.response.html import HtmlResponse
 
 
 class WorldometersSpider(scrapy.Spider):
-    name = "worldometers"
-    allowed_domains = ["www.worldometers.info"]
-    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+    name: str = "worldometers"
+    allowed_domains: list[str] = ["www.worldometers.info"]
+    start_urls: list[str] = [
+        "https://www.worldometers.info/world-population/population-by-country"
+    ]
 
-    def parse(self, response):
-        pass
+    def parse(
+            self,
+            response: HtmlResponse
+            ) -> Generator[dict[str, Any], Any, None]:
+        title: str | None = response.xpath('//h1/text()').get()
+        countries: list[str] = response.xpath('//td/a/text()').getall()
+
+        yield {
+            'title': title,
+            'countries': countries,
+        }
