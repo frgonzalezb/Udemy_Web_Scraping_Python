a599d9d17649b1744f347ac063e343fad94fd9b6
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-10 16:52:50 -0400

    Crawlers - Reglas (Rules)

---
+++ 09_ScrapyCrawlers/scrawlers/scrawlers/spiders/transcripts.py
@@ -0,0 +1,36 @@
+from typing import Any, Generator, Literal
+
+import scrapy
+from scrapy.http.response.html import HtmlResponse
+from scrapy.selector.unified import SelectorList, Selector
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class TranscriptsSpider(CrawlSpider):
+    name: Literal['transcripts'] = "transcripts"
+    allowed_domains: list[str] = ["subslikescript.com"]
+    start_urls: list[str] = ["https://subslikescript.com/movies"]
+
+    xpath: str = '//ul[contains(@class, "scripts-list")]/li/a'
+    rules: tuple[Rule] = (
+        Rule(
+            LinkExtractor(restrict_xpaths=xpath),
+            callback="parse_item",
+            follow=True
+        ),
+    )
+
+    custom_settings: dict[str, Any] = {
+        'DOWNLOAD_DELAY': 0.5,  # Fija un retraso de 0.5 segundos
+    }
+
+    def parse_item(self, response: HtmlResponse):
+        article_xpath: str = '//article[contains(@class, "main-article")]'
+        article: SelectorList[Selector] = response.xpath(article_xpath)
+        yield {
+            'title': article.xpath('./h1/text()').get(),
+            'plot': article.xpath('./p').get() if article.xpath('./p') else '',
+            'script': article.xpath('./div[@class="full-script"]').getall(),
+            'url': response.url
+        }
