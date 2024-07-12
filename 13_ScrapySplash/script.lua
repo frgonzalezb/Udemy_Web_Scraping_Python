cc46f1adf169d216a84e6c6cc70df2bc3843cd39
Francisco <fr.gonzalezb@duocuc.cl>
2024-07-12 19:17:02 -0400

    Cambiar el User-Agent

--- 13_ScrapySplash/script.lua
+++ 13_ScrapySplash/script.lua
@@ -1,6 +1,12 @@
 -- Página a usar: https://www.adamchoi.co.uk/overs/detailed
 
 function main(splash, args)
+    -- splash:user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    headers = {
+        ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    }
+    splash:set_custom_headers(headers)
+    
     splash.private_mode_enabled = false
 
     assert(splash:go(args.url))
